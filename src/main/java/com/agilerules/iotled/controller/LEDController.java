d533da4c21ebaa243879c4c83e95e71059970486
andrej <andrejjovanov@outlook.com>
2018-07-16 13:40:11 +0200

    changed pin

--- src/main/java/com/agilerules/iotled/controller/LEDController.java
+++ src/main/java/com/agilerules/iotled/controller/LEDController.java
@@ -77,7 +77,7 @@ public class LEDController {
 	public String light(){
 		if(pin==null){
 			GpioController gpio = GpioFactory.getInstance();
-			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_18,"My LED",PinState.LOW);
+			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01,"My LED",PinState.LOW);
 		}
 		pin.toggle();
 		return "Response from light!!";
